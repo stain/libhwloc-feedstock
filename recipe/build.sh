6d4c36f9a51334bac57ed26e53c8a163fddb9d81
Isuru Fernando <isuruf@gmail.com>
2018-09-22 00:52:46 -0500

    Enable shared on OSX

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,7 +8,7 @@ DISABLES="--disable-cairo --disable-opencl --disable-cuda --disable-nvml"
 DISABLES="$DISABLES --disable-gl --disable-libnuma --disable-libudev"
 
 if [ `uname` == Darwin ]; then
-    ./configure --prefix=$PREFIX --disable-shared $DISABLES
+    ./configure --prefix=$PREFIX $DISABLES
 else
     export LDFLAGS="${LDFLAGS} -Wl,--as-needed"
     ./configure --prefix=$PREFIX $DISABLES
