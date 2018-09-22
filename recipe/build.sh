9119816f1fc22c21dbf6bb76537fbdfb47a5c816
Isuru Fernando <isuruf@gmail.com>
2018-09-22 10:48:33 -0500

    Add windows support

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,18 +2,41 @@
 
 set -e
 
-chmod +x configure
-
 DISABLES="--disable-cairo --disable-opencl --disable-cuda --disable-nvml"
 DISABLES="$DISABLES --disable-gl --disable-libnuma --disable-libudev"
 
-if [ `uname` == Darwin ]; then
-    ./configure --prefix=$PREFIX $DISABLES
-else
-    export LDFLAGS="${LDFLAGS} -Wl,--as-needed"
-    ./configure --prefix=$PREFIX $DISABLES
-fi
+chmod +x configure
 
-make
-make check
+case `uname` in
+    Darwin)
+        ./configure --prefix=$PREFIX $DISABLES
+        ;;
+    Linux)
+        export LDFLAGS="${LDFLAGS} -Wl,--as-needed"
+        ./configure --prefix=$PREFIX $DISABLES
+        ;;
+    MINGW*)
+        export PATH="$PREFIX/Library/bin:$BUILD_PREFIX/Library/bin:$PATH"
+        export CC=clang-cl
+        export RANLIB=llvm-ranlib
+        export AS=llvm-as
+        export AR=llvm-ar
+        export LD=link
+        export CFLAGS="-MD -I$PREFIX/Library/include -O3"
+        export CXXFLAGS="-MD -I$PREFIX/Library/include -O3"
+        export LDFLAGS="$LDFLAGS -L$PREFIX/Library/lib"
+        sed -i "s/-Wl,-DLL,-IMPLIB/-link -DLL -IMPLIB/g" configure
+        ./configure --prefix="$PREFIX/Library" --libdir="$PREFIX/Library/lib" $DISABLES
+        ;;
+esac
+
+make -j${CPU_COUNT}
+make check -j${CPU_COUNT}
 make install
+
+PROJECT=hwloc
+if [[ `uname` == MINGW* ]]; then
+    LIBRARY_LIB=$PREFIX/Library/lib
+    mv "${LIBRARY_LIB}/${PROJECT}.lib" "${LIBRARY_LIB}/${PROJECT}_static.lib"
+    mv "${LIBRARY_LIB}/${PROJECT}.dll.lib" "${LIBRARY_LIB}/${PROJECT}.lib"
+fi
